78a56612a63cc8b0b2b81d8e1d7eb2165e89230a
Roger Heim <roger.heim@gmail.com>
2011-04-30 14:24:02 -0400

    Initial commit

---
+++ VideoPlayList/src/com/hackathon/StartActivity.java
@@ -0,0 +1,18 @@
+package com.hackathon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: roger
+ * Date: 4/30/11
+ * Time: 2:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.playlist_layout);
+    }
+}
