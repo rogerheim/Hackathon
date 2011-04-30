7ad042a5ce986469a8409c799bf8d8fa8fbaecc3
Roger Heim <roger.heim@gmail.com>
2011-04-30 14:59:04 -0400

    Initial framework built

--- VideoPlayList/src/com/hackathon/StartActivity.java
+++ VideoPlayList/src/com/hackathon/StartActivity.java
@@ -1,18 +1,79 @@
 package com.hackathon;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
 
-/**
- * Created by IntelliJ IDEA.
- * User: roger
- * Date: 4/30/11
- * Time: 2:17 PM
- * To change this template use File | Settings | File Templates.
- */
+import java.util.ArrayList;
+
 public class StartActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.playlist_layout);
+    }
+
+
+
+    class VideoAdapter extends BaseAdapter {
+
+        private LayoutInflater inflater;
+        private ArrayList<YouTubeVideo> myVideos;
+
+        public VideoAdapter(Context ctx) {
+            inflater = LayoutInflater.from(ctx);
+        }
+
+        @Override
+        public int getCount() {
+            return myVideos.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return myVideos.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return myVideos.get(position).getId();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            VideoViewHolder holder;
+            YouTubeVideo vid = myVideos.get(position);
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.playlist_item_layout, null);
+                holder = new VideoViewHolder();
+                holder.thumbnailIV = (ImageView) convertView.findViewById(R.id.video_thumbnail);
+                holder.titleTV = (TextView) convertView.findViewById(R.id.video_title);
+                holder.title2TV = (TextView) convertView.findViewById(R.id.video_title_line_2);
+                convertView.setTag(holder);
+            } else {
+                holder = (VideoViewHolder) convertView.getTag();
+            }
+            holder.id = vid.getId();
+            holder.titleTV.setText(vid.getVideoTitle());
+            holder.title2TV.setText(vid.getVideoTitle2());
+            //TODO: set image view
+
+            return convertView;
+        }
+    }
+
+    class VideoViewHolder {
+        public long id;
+        public ImageView thumbnailIV;
+        public TextView titleTV;
+        public TextView title2TV;
+        
     }
 }
---
+++ VideoPlayList/src/com/hackathon/YouTubeVideo.java
@@ -0,0 +1,32 @@
+package com.hackathon;
+
+public class YouTubeVideo {
+
+    private long id;
+    private String videoTitle;
+    private String videoTitle2;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getVideoTitle2() {
+        return videoTitle2;
+    }
+
+    public void setVideoTitle2(String videoTitle2) {
+        this.videoTitle2 = videoTitle2;
+    }
+
+    public String getVideoTitle() {
+        return videoTitle;
+    }
+
+    public void setVideoTitle(String videoTitle) {
+        this.videoTitle = videoTitle;
+    }
+}
